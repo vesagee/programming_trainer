913decfb0b560dd9cb6286c77c8ea2e33f2ac38d
Vesa <vesagee@gmail.com>
2025-09-04 18:04:20 +0300

    update files

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    user_input = None
+    if request.method == 'POST':
+        user_input = request.form.get('user_input')
+    return render_template('index.html', user_input=user_input)
+
+if __name__ == '__main__':
+    app.run(debug=True)
